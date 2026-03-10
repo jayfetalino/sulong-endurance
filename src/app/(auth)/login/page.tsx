32bce7976940aa5325d0fb27d1b8bf58735e3598
jayfetalino <jayfetalino@gmail.com>
2026-03-09 22:58:40 -0400

    Crop logo tightly - no more empty space

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -50,7 +50,7 @@ export default function LoginPage() {
 
     {/* Logo */}
      <div style={{ textAlign: 'center', marginBottom: '16px' }}>
-          <img src="/logo.png" alt="Sulong" style={{ width: '360px', height: 'auto', marginBottom: '0px' }} />
+          <img src="sulong-logo-transparent.png" alt="Sulong" style={{ width: '360px', height: 'auto', marginBottom: '0px' }} />
           <div style={{
             fontFamily: 'DM Sans, sans-serif',
             fontSize: '0.6rem',
